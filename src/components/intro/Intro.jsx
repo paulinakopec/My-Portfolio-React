c82a3f66cd72009dbde428a9750c1aaebb61e92f
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-19 22:22:27 +0200

    Intro jsx file update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,87 +1,37 @@
 import React from 'react'
 import './intro.css'
 import Me from '../../img/me.png'
+import Arrow from '../../img/arrow.png'
 
 const Intro = () => {
   return (
     <div className='i'>
         <div className="i-left">
             <div className="i-left-wrapper">
-                <h2 className="i-intro">Hello, My name is</h2>
-                <h1 className="i-name">Paulina Kopec</h1>
-                <div className="i-title">
-                    <div className="i-title-wrapper">
-                        <div className="i-title-item">Front-End Developer</div>
-                        <div className="i-title-item">Future Fullstack Developer</div>
-                        <div className="i-title-item">Former Financial Analyst</div>
-                        <div className="i-title-item">Self-taught Ceramist</div>
-                        <div className="i-title-item">Basketball Player</div>
-                    </div>
+                <div className="i-left-upper-wrapper">
+                  <h2 className="i-intro">Hi! My name is</h2>
+                  <h1 className="i-name">Paulina Kopec</h1>
+                  <div className="i-title">
+                      <div className="i-title-wrapper">
+                          <div className="i-title-item">Front-End Developer</div>
+                          <div className="i-title-item">Postgraduate Student</div>
+                          <div className="i-title-item">Financial Analyst</div>
+                          <div className="i-title-item">Self-taught Ceramist</div>
+                          <div className="i-title-item">Basketball Player</div>
+                      </div>
+                  </div>
+                  <p className="i-desc">
+                      Self-taught <b>Front-End Developer with a strong financial background</b> in Commercial Controlling in FMCG, Audit in Big4 and Accounting in Automotive company. 
+                      <b> Postgraduate student of Front-End Development with React</b> at Kozminski University. Master and Bachelor graduate of Finance and Accounting course at Warsaw School of Economics. 
+                      Open-minded and a fast learner with <b>good analytical and problem solving skills.</b>
+                  </p>
                 </div>
-                <p className="i-desc">
-                    Self-taught Front-End Developer with a strong financial background. 
-                    I am starting my career path in IT, as I am seeking for continuous development and I have very good analytical and problem solving skills. 
-                    I consider myself as an open-minded, highly-motivated person and a fast learner. 
-                    I am experienced in working on projects, with the client and in a team.
-                </p>
+                <div className="i-left-lower-wrapper">
+                  <a href="#s" className="lets-connect-link"><p className="i-text-connect">Find out more about me</p></a>
+                  <a href="#s"><img src = {Arrow} alt="" className="i-arrow"/></a>
+                </div>
             </div>
         </div>
-        <svg
-          width="75"
-          height="75"
-          viewBox="0 0 75 75"
-          fill="none"
-          stroke="pink"
-          className="i-scroll"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="scroll">
-            <path
-              id="Vector"
-              d="M40.5 15L34.5 9L28.5 15"
-              stroke-width="3"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              id="Vector_2"
-              d="M28.5 24L34.5 30L40.5 24"
-              stroke-width="3"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <g id="Group">
-              <path
-                id="Vector_3"
-                d="M9 37.5H60"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-            <path
-              id="Vector_4"
-              d="M34.5 27V9"
-              stroke-width="2.9895"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <g id="Group_2">
-              <path
-                id="Vector_5"
-                d="M9 27C9 12.918 20.418 1.5 34.5 1.5C48.5859 1.5 60 12.918 60 27C60 29.8906 60 45.1094 60 48C60 62.082 48.5859 73.5 34.5 73.5C20.418 73.5 9 62.082 9 48C9 45.1094 9 29.8906 9 27Z"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-          </g>
-        </svg>
         <div className="i-right">
             <div className="i-bg"></div>
             <img src={Me} alt="" className="i-img"/>
