9e5a5ddea09ad010216bf6f944f870ebee2c2c99
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-19 22:31:43 +0200

    Adding product list and product component

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './productList.css';
+import Product from '../product/Product';
+import { products } from '../../data';
+
+const ProductList = () => {
+  return (
+    <div className = "pl">
+        <div className="pl-texts">
+            <h1 className="pl-title">Works and projects</h1>
+            <p className="pl-desc">
+                Websites presented below were built using HTML, CSS, JavaScript and React.
+                For more details, please click on a project to view the website or check out my <a href="https://github.com/paulinakopec" target="_blank" rel="noreferrer" className="pl-github-link">GitHub</a> account.
+            </p>
+        </div>
+        <div className="pl-list">
+          {products.map((item) => (
+            <Product key={item.id} img={item.img} link={item.link}/>
+          ))}
+        </div>
+    </div>
+  )
+}
+
+export default ProductList
