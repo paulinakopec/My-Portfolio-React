24982d97dc0593883313c2b24e4fa802ba95107c
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-19 22:37:45 +0200

    Adding data.js file

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import TravelBlogWebsite from '../src/img/travelblog.png';
+import CeramicsEcommerceWebsite from '../src/img/ceramicsshop.png';
+import NetflixClone from '../src/img/dulu.png';
+import WeatherWorldWide from '../src/img/weatherworldwide.png';
+
+export const products = [
+    {
+        id: 1,
+        img: TravelBlogWebsite,
+        link: 'https://smallwavestravel.netlify.app/',
+    },
+    {
+        id: 2,
+        img: CeramicsEcommerceWebsite,
+        link:'https://smallwavesceramics.netlify.app/',
+    },
+    {
+        id: 3,
+        img: NetflixClone,
+        link: 'https://dulu.netlify.app/',
+    },
+    {
+        id: 4,
+        img: WeatherWorldWide,
+        link:'https://weatherworldwide.netlify.app/',
+    },
+]
