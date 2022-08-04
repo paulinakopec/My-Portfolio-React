ed0efadb0573eed08c487ce37fcdabc4ffca77b0
paulinakopec <paulinakopec32@gmail.com>
2022-08-04 12:11:26 +0200

    Adding projects to data file

--- src/data.js
+++ src/data.js
@@ -2,25 +2,38 @@ import TravelBlogWebsite from '../src/img/travelblog.png';
 import CeramicsEcommerceWebsite from '../src/img/ceramicsshop.png';
 import NetflixClone from '../src/img/dulu.png';
 import WeatherWorldWide from '../src/img/weatherworldwide.png';
+import Nofomo from '../src/img/nofomo.png';
+import Ballin from '../src/img/ballin.png';
 
 export const products = [
     {
         id: 1,
+        img: Nofomo,
+        link: 'https://nofomo.netlify.app/',
+    },
+    {
+        id: 2,
         img: TravelBlogWebsite,
         link: 'https://smallwavestravel.netlify.app/',
     },
     {
-        id: 2,
+        id: 3,
+        img: Ballin,
+        link: 'https://ballinblog.netlify.app/',
+
+    },
+    {
+        id: 4,
         img: CeramicsEcommerceWebsite,
         link:'https://smallwavesceramics.netlify.app/',
     },
     {
-        id: 3,
+        id: 5,
         img: NetflixClone,
         link: 'https://dulu.netlify.app/',
     },
     {
-        id: 4,
+        id: 6,
         img: WeatherWorldWide,
         link:'https://weatherworldwide.netlify.app/',
     },
