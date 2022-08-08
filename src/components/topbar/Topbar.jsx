7906a481f080cd3778ca15a627d7f911c710096d
paulinakopec <paulinakopec32@gmail.com>
2022-08-08 14:16:24 +0200

    Adding drop-down menu for mobiles and tablets

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './topbar.css';
 
-const Topbar = () => {
-  return (
-    <div className='topbar'>
-        <ul className="topList">
-            <li className="topListItem"><a href="#i" className="link">HOME</a></li>
-            <li className="topListItem"><a href="#s" className="link">SKILLS</a></li>
-            <li className="topListItem"><a href="#pl" className="link">PROJECTS</a></li>
-            <li className="topListItem"><a href="#c" className="link">CONTACT</a></li>
-        </ul>
-    </div>
-  )
+class Topbar extends Component {
+  state = { clicked: false } 
+  
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
+
+  render () {
+    return (
+        <div className='topbar'>
+            <div className="menuIcon" onClick={this.handleClick}>
+                <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+            <ul className={this.state.clicked ? "topList active" : "topList"}>
+                <li className="topListItem"><a href="#i" className="link">HOME</a></li>
+                <li className="topListItem"><a href="#s" className="link">SKILLS</a></li>
+                <li className="topListItem"><a href="#pl" className="link">PROJECTS</a></li>
+                <li className="topListItem"><a href="#c" className="link">CONTACT</a></li>
+            </ul>
+        </div>
+    )
+  }
 }
 
 export default Topbar
